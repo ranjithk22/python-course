67428738a49a902767b409a8542c0fb006e7a8c1
ranjithk22 <ranjithkumar.k22@gmail.com>
2024-07-16 13:11:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# print('Hello, My name is Ranjith Kumar')
+
+# print('Hello World')
+# print('Hello World')
+# print('Hello World')
+
+# print('Hello World\nHello World\nHello World')
+
+# String concatenation
+# print('Hello' + 'World')
+# print('Hello' + ' ' + 'World')
+
+# Indentation Error
+    # print('hello world') 
+
+# Input Function
+# print('Hello, my name is '+  input('what is you name? '))
+
+# string
+# number
+
+# name = input('What is your name? ')
+# print('My name is ' + name)
+
+# name = 'Kumar'
+# print(name)
+
+# input(len(input('What is your name')))
+# name = input('what is you name? ')
+# length = len(name)
+# print(length)
+
